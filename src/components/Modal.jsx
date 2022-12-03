d4c1a7fbc26303f0e08528e02b4a8a25b09d1bb6
hoas22 <114961666+hoas22@users.noreply.github.com>
2022-12-03 06:52:18 -0500

    Add files via upload
    LOGIN / RESPONSIVE INCORPORADOS

---
+++ src/components/Modal.jsx
@@ -0,0 +1,16 @@
+import "../styles/modal.css"
+
+const Modal = ({ isOpen, onClose, children }) => {
+
+  return (
+    <div className="modal-container" style={{ display: isOpen ? 'grid' : 'none' }}>
+      <div className="modal-body ">
+        <button className="modal-close close" onClick={onClose}>CERRAR</button>
+        {children}
+        <button className="close" onClick={onClose}>CERRAR</button>
+      </div>
+    </div>
+  )
+}
+
+export default Modal
