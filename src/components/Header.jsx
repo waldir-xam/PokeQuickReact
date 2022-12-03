72e08ffa2d2a1e92f91648c578fc62cc3003a39c
Waldir <waliuxd@gmail.com>
2022-12-02 20:22:31 -0500

    avance

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,35 @@ import pokebola from "../img/pokebola_logo.svg";
 import "../styles/Header.css";
 
 const Header = () => {
+  const menuBtn = document.getElementById("menuBtn");
+  const body = document.body;
+
+/*   menuBtn.addEventListener("click", () => {
+  body.classList.toggle("show__menu"); 
+  })*/;
+
   return (
     <div>
-      <nav class="nav">
-        <img className="logo" src={pokebola} alt=""/>
+      <div class="header">
+        <nav>
+          {/*           <img className="logo" src={pokebola} alt="" />
+           */}
+          <button className="nav-menu-btn" id="menuBtn"></button>
+          <ul class="navbar">
+            <li>
+              <a href="">Inicio</a>
+            </li>
+            <li>
+              <a href="">Pokedex</a>
+            </li>
+            <li>
+              <a href="">Mis Pokemons</a>
+            </li>
+            <li>
+              <a href="">Mochila</a>
+            </li>
+          </ul>
+        </nav>
         <figure class="logo-container">
           <img class="logo" src={pokelogo} alt="logo" />
         </figure>
@@ -15,7 +40,7 @@ const Header = () => {
           <span>BGM ON/OFF</span>
           <i class="fa-solid fa-play"></i>
         </div>
-      </nav>
+      </div>
     </div>
   );
 };
