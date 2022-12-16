ffb60c1157f7aff964e5a4eee4cfe3d9e2ac7a74
hoas22 <114961666+hoas22@users.noreply.github.com>
2022-12-15 21:08:51 -0500

    Add files via upload
    UPDATE

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,9 +3,9 @@ import "../styles/modal.css"
 const Modal = ({ isOpen, onClose, children }) => {
 
   return (
-    <div className="modal-container" style={{ display: isOpen ? 'grid' : 'none' }}>
+    <div className="modal-container" style={{ display: isOpen ? 'grid' : 'none', zIndex:1}}>
       <div className="modal-body ">
-        <button className="modal-close close" onClick={onClose}>CERRAR</button>
+        <button className="modal-close close" onClick={onClose} >CERRAR</button>
         {children}
         <button className="close" onClick={onClose}>CERRAR</button>
       </div>
