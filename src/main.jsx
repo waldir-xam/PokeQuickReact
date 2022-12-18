58d7511ab713ddbc412ac106cb9951951b74e1b9
Slax2 <moiseslax@hotmail.com>
2022-12-18 18:57:24 -0500

    prueba

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,7 @@ import Router from "./router/Router";
 import { AuthProvider } from "./context/UserContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <Router />
-    </AuthProvider>
-  </React.StrictMode>
+  <AuthProvider>
+    <Router />
+  </AuthProvider>
 );
