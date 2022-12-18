bcedaf2c694432dbfe39c377bb1965f779c8fffa
Waldir <waliuxd@gmail.com>
2022-12-18 17:29:29 -0500

    go

---
+++ src/components/AudioPlayer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import AudioPlayer from "react-audio-player";
+
+function AudioPlayerButton() {
+  const audioPlayer = React.useRef();
+
+  return (
+    <div>
+      <AudioPlayer
+        src="https://vgmsite.com/soundtracks/pokemon-heartgold-and-soulsilver/qpgcaxnrmz/002%20Title%20Screen.mp3"
+        controls
+        ref={audioPlayer}
+        autoPlay
+      />
+      <button onClick={() => audioPlayer.current.audioEl.play()}>
+        <i className="fa-solid fa-play"></i>
+      </button>
+    </div>
+  );
+}
+
+export default AudioPlayerButton;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,60 +1,42 @@
 import React from "react";
-import { useState } from 'react'
+import { useState } from "react";
 import Modal from "../components/Modal";
-import Login from "../components/Login"
+import Login from "../components/Login";
 import Modales from "../components/Modales";
 import pokelogo from "../img/poke-logo.png";
 import pokebola from "../img/pokebola_logo.svg";
 import "../styles/Header.css";
+import AudioPlayer from "../components/AudioPlayer"
 
 const Header = () => {
-
-  const [mostrar, setMostrar] = useState(false)
-  const [verContador, setVerContador] = useState(false)
-  const [verFormulario, setVerFormulario] = useState(false)
-
+  const [mostrar, setMostrar] = useState(false);
+  const [verContador, setVerContador] = useState(false);
+  const [verFormulario, setVerFormulario] = useState(false);
 
   const menuBtn = document.getElementById("menuBtn");
   const body = document.body;
 
-/*   menuBtn.addEventListener("click", () => {
-  body.classList.toggle("show__menu"); 
-  })*/;
+
   return (
     <div>
       <div className="header">
         <nav>
-          {/*           <img className="logo" src={pokebola} alt="" />
-           */}
-          <button className="nav-menu-btn" id="menuBtn" onClick={() => setMostrar(true)}></button>
-          
+          <button
+            className="nav-menu-btn"
+            id="menuBtn"
+            onClick={() => setMostrar(true)}
+          ></button>
+
           <Modal isOpen={mostrar} onClose={() => setMostrar(false)}>
-            
-            <Login/>
-            
+            <Login />
           </Modal>
-          
-{/*           <ul class="navbar">
-            <li>
-              <a href="">Inicio</a>
-            </li>
-            <li>
-              <a href="">Pokedex</a>
-            </li>
-            <li>
-              <a href="">Mis Pokemons</a>
-            </li>
-            <li>
-              <a href="">Mochila</a>
-            </li>
-          </ul> */}
         </nav>
         <figure className="logo-container">
           <img className="logo" src={pokelogo} alt="logo" />
         </figure>
         <div className="nav__sound">
           <span>BGM ON/OFF</span>
-          <i className="fa-solid fa-play"></i>
+         <AudioPlayer/>
         </div>
       </div>
     </div>
