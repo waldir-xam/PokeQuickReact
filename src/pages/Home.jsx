1efd55cc91e74ea7440d68bdfcd3aa1c14403f40
Waldir <waliuxd@gmail.com>
2022-12-17 22:26:58 -0500

    linkpt2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,7 @@ import asideOne from "../img/aside1.png";
 import asideTwo from "../img/aside2.png";
 import asideThree from "../img/aside3.png";
 import asideFour from "../img/aside4.png";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -62,7 +63,12 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <button className="nextStep">Siguiente <i class="fa-solid fa-arrow-right"></i></button> <FooterOne />
+        <Link to="/stage2">
+          <button className="nextStep">
+            Siguiente <i class="fa-solid fa-arrow-right"></i>
+          </button>
+        </Link>
+        <FooterOne />
         <div className="aside-left-home">
           <img src={asideOne} className="aside-img-home" />
           <img src={asideTwo} className="aside-img-home" />
