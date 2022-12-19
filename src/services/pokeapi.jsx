83c78866222da6d0386e8e5297031f3a8cd8388e
Slax2 <moiseslax@hotmail.com>
2022-12-18 19:00:35 -0500

    prueba

--- src/services/pokeapi.jsx
+++ src/services/pokeapi.jsx
@@ -1,19 +1,15 @@
-
-const searchPokemon = async(pokemon) => {
+import React from "react";
+const searchPokemon = async (pokemon) => {
   try {
     const response = await fetch(
-        `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+      `https://pokeapi.co/api/v2/pokemon/${pokemon}`
     );
     const data = await response.json();
 
     return data;
-    
   } catch (error) {
-
     return error;
-
   }
-  
-}
+};
 
-export default searchPokemon
+export default searchPokemon;
